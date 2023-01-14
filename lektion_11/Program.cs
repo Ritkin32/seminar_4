411a40c68f97a2a189b8938200bfd450dfeaf94b
rita <AdaRitDN@mail.ru>
2023-01-14 23:39:22 +0300

    массив код

--- lektion_11/Program.cs
+++ lektion_11/Program.cs
@@ -1,2 +1,82 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Урок 4. Функции
+
+// 1. Ввод N  int <- (string)
+int GetVal(string text)
+{
+Console.Write("Введите числo:");
+return Convert.ToInt32(Console.ReadLine());
+}
+
+// 2 .Создание массива
+int[] CreateArray(int size)
+{
+int[] array = new int[size];
+return array;
+}
+
+// 3. Заполнение массива 0 и 1
+void FillArray(int[] collection)
+{
+  int length = collection.Length;
+  int index = 0;
+  collection[0] = 1;
+  while (index < length)
+  {
+    collection[index] = new Random().Next(0, 1);
+    index++;
+  }
+}
+
+
+//4/ Печать массива
+
+void PrintArray(int[] col)
+{
+ int count = col.Length;
+
+ for (int i = 0; i < count; i++)
+ {
+    Console.Write($"{col[i]}");
+ }
+ Console.WriteLine();
+}
+//5 конвертация из 2 в 10
+
+int Binarr(int[] arrBin)
+{
+     int count = arrBin.Length;
+     int exponent = arrBin.Length - 1;
+     int result = 0;
+     for (int i = 0; i < arrBin.Length; i++)
+     {
+         if (arrBin[i] == 1)
+         {
+            result += Convert.ToInt32(Math.Pow(2, exponent));
+         }
+         exponent--;
+     }
+     return result;
+}    
+
+
+PrintArray(array);
+FillArray(array);
+PrintArray(array);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lekttion2/Program.cs
@@ -0,0 +1,50 @@
+﻿{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива:");
+            int arrSize = Convert.ToInt32(Console.ReadLine());
+
+            var myArray = CreateArray(arrSize);
+
+            Console.WriteLine($"Массив: {string.Join(", ", myArray)}");
+
+            string binLine = string.Join("", myArray);
+
+            Console.WriteLine($"{binLine} переводим в десятичное: {ToDec(binLine)}");
+
+            Console.ReadLine();
+        }
+
+        static int[] CreateArray(int arrSize)
+        {
+            Random rnd = new Random(); 
+
+            int[] myArray = new int[arrSize];
+
+            myArray[0] = 1; 
+
+            for (int i = 1; i < arrSize; i++)
+            {
+                myArray[i] = rnd.Next(0, 2);
+            }
+
+            return myArray;
+        }
+
+        public static int ToDec(string bin)
+        {
+            var result = 0;
+            for (int i = 0; i < bin.Length; ++i)
+            {
+                if (bin[bin.Length - i - 1] == '1')
+                {
+                    result += (int)Math.Pow(2, i);
+                }
+            }
+            return result;
+        }
+
+    }
+}
